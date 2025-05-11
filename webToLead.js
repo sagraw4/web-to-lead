b2dc474d27d78b6fa35174c4d1dabcad853d8c5c
Surabhi <surabhiagrawal91@gmial.com>
2025-05-10 21:11:59 -0400

    commit

---
+++ webToLead.js
@@ -0,0 +1,26 @@
+console.log("Web To Lead");
+/*const today = new Date();
+const year = today.getFullYear();
+const month = String(today.getMonth() + 1).padStart(2, '0'); // Month is 0-indexed
+const day = String(today.getDate()).padStart(2, '0');
+const formattedDate = `${year}-${month}-${day}`;
+document.getElementById('00NKk000001FRNu').value = formattedDate;
+console.log("formattedDate", formattedDate);
+*/
+
+
+function beforeSubmit(){
+
+    let inputdate = document.querySelector(".inputdate");
+    let outputdate = document.querySelector(".outputdate");
+    console.log('inputdate.value',inputdate.value );
+    let formattedDate = new Date(inputdate.value);
+    let utcDateString = formattedDate.toISOString();
+    const formattedDateEST = new Date(utcDateString).toLocaleDateString('en-US', {
+        timeZone: 'America/New_York', // Replace with the desired time zone
+      });  
+
+    console.log("formattedDateEST", formattedDateEST);
+    outputdate.value = formattedDateEST;
+
+}
