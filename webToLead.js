b650a20b5018f977d6f9b20e1ef07af4a4eda3f1
Surabhi <surabhiagrawal91@gmial.com>
2025-05-11 18:50:44 -0400

    function added to prevent submission before recaptcha

--- webToLead.js
+++ webToLead.js
@@ -8,9 +8,13 @@ document.getElementById('00NKk000001FRNu').value = formattedDate;
 console.log("formattedDate", formattedDate);
 */
 
+let captchaVerified = false;
 
-function beforeSubmit(){
+function beforeSubmit(event){
 
+ 
+  if (captchaVerified){
+   
     let inputdate = document.querySelector(".inputdate");
     let outputdate = document.querySelector(".outputdate");
     console.log('inputdate.value',inputdate.value );
@@ -22,7 +26,15 @@ function beforeSubmit(){
 
     console.log("formattedDateEST", formattedDateEST);
     outputdate.value = formattedDateEST;
-
+  }
+  else {
+    alert("Please verify reCAPTCHA box to submit the lead");
+    event.preventDefault(); 
+  }
 }
 
 function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500); 
+
+function captchaVerifiedSuccess(){
+  captchaVerified =true;
+}
